00c0b558dd8fe300a2173de60dbf44489e25463b
ninox92 <thomasreynders@hotmail.nl>
2015-10-19 16:28:42 +0200

    Virtual passage

--- Rogue-text-adventure/Passage.h
+++ Rogue-text-adventure/Passage.h
@@ -9,8 +9,12 @@ class Passage
 	std::map<Direction, Room*> map;
 public:
 	Passage();//Default Constructor
-	~Passage();
+	virtual ~Passage();
 
+
+	virtual std::string Identify() = 0;//PURE abstract function
+	//To override use:
+	// virtual std::string Identify() override { return "";//new content here }
 
 	void Add(Room* r, Direction d);
 
